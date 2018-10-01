5c491a544380b4b48e2635cc55e1ae3fb2106da9
Anna Jankowska <pwr218187@gmail.com>
2018-10-01 19:05:40 +0200

    Created a servlet that downloads and displays parameters

---
+++ src/Servletes/ParametersServlet.java
@@ -0,0 +1,19 @@
+package Servletes;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/parameters")
+public class ParametersServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String surname = req.getParameter("surname");
+
+        resp.getWriter().write("Hello friend " + name + " " + surname);
+    }
+}
